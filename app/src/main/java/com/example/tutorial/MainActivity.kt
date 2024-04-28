51bfe77946377564af624c95311a404d55707eb6
LastWord69ANIMA <90632303+LastWord69ANIMA@users.noreply.github.com>
2024-04-29 01:25:49 +0900

    Initial commit.

---
+++ app/src/main/java/com/example/tutorial/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.tutorial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView: TextView = findViewById(R.id.text)
+        val button: Button = findViewById(R.id.button)
+        button.setOnClickListener {
+            textView.text = "Tapされたよ"
+        }
+    }
+}
